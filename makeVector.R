f1e88834141b73369f8b22beda892d870297148b
ravisls <87ravi.r@gmail.com>
2014-05-25 07:54:49 -0700

    Update and rename README.md to makeVector.R

---
+++ makeVector.R
@@ -0,0 +1,13 @@
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
